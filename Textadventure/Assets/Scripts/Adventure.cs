b646939a11e049edf627678a00d37a6d1b89b1dd
ChristianALGomez <10721401@my.uvu.edu>
2018-01-24 10:00:07 -0700

    Text adventure, begain code for each state, Need to use own story

--- Textadventure/Assets/Scripts/Adventure.cs
+++ Textadventure/Assets/Scripts/Adventure.cs
@@ -4,20 +4,90 @@ using UnityEngine;
 
 public class Adventure : MonoBehaviour {
 
-    public enum States { cell, window, gate};
-    public States myState;
+    // change States to own states for knight game
+    public enum States { cell, window, gate, bed};
+    public States currentState;
 
 
     public int number;
-    
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Use this for initialization
+    void Start() {
+        currentState = States.cell;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentState == States.cell)
+        {
+            Cell();
+        }
+        else if (currentState == States.bed)
+        {
+            Bed();
+        }
+        else if (currentState == States.window)
+        {
+            Window();
+        }
+        else if (currentState == States.gate)
+        {
+            Gate();
+        }
+
+    }
+
+    private void Cell()
+    {
+        print("You are in a cell, it is small dark and stinky. \n" +
+            "There is a Window. \n" +
+            "There is a Gate. \n" +
+            "There is a Bed. \n" +
+            "Press W for Window, G for Gate, B for Bed \n");
+        
+        if(Input.GetKeyDown(KeyCode.W)) { currentState = States.window; }
+        if(Input.GetKeyDown(KeyCode.G)) { currentState = States.gate; }
+        if(Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
+    }
+    
+    private void Bed()
+    {
+        print("The bed is moldy, torn, and the sorce of the smell. \n" +
+            "There is a Window. \n" +
+            "There is a Gate. \n" +
+            "There is a Cell. \n" +
+            "Press W for Window, G for Gate, C for Cell. \n");
+
+        if (Input.GetKeyDown(KeyCode.W)) { currentState = States.window; }
+        if (Input.GetKeyDown(KeyCode.G)) { currentState = States.gate; }
+        if (Input.GetKeyDown(KeyCode.C)) { currentState = States.cell; }
+    }
+
+    private void Window()
+    {
+        print("There are bars on the window, outside you can see a highwall, trees, and two gaurds. \n" +
+            "There is a Gate. \n" +
+            "There is a Bed. \n" +
+            "There is a Cell. \n" +
+            "Press G for Gate, B for Bed, C for Cell. \n");
+
+        if (Input.GetKeyDown(KeyCode.G)) { currentState = States.gate; }
+        if (Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
+        if (Input.GetKeyDown(KeyCode.C)) { currentState = States.cell; }
+    }
+    
+    private void Gate()
+    {
+        print("It is old and made out of wood, the hinges are very rusty. \n" +
+            "There is a Bed. \n" +
+            "There is a Window. \n" +
+            "There is a Cell. \n" +
+            "Press B for bed, W for window, C for Cell. \n");
+
+        if (Input.GetKeyDown(KeyCode.W)) { currentState = States.window; }
+        if (Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
+        if (Input.GetKeyDown(KeyCode.C)) { currentState = States.cell; }
+    }
 }
