49b0e67a7230d2f2a491111536b715189bff11f1
ChristianALGomez <10721401@my.uvu.edu>
2018-02-07 10:21:36 -0700

    Added bouncy ball, HP, and brick object

---
+++ BrickBreaker/Assets/Scripts/HP.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour {
+
+    public int health;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health--;
+
+
+        //if our health gets to zero
+        if(health <= 0)
+        {
+            //then destroy this object
+            Destroy(gameObject);
+        }
+    }
+
+}
