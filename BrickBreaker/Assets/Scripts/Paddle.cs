718a8015c01f81b4676dce0fec2e35a0d072a016
ChristianALGomez <10721401@my.uvu.edu>
2018-02-12 10:15:18 -0700

    Paddle added, need sound effects, and score board

---
+++ BrickBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    void Update()
+    {
+        //get current paddle positiion
+        Vector3 paddlePosition = new Vector3(0, gameObject.transform.position.y, 0);
+
+        //get mouse horizontal position
+        float mousePositionInBlocks = Input.mousePosition.x / Screen.width * 15 - 7.5f;
+
+        paddlePosition.x = mousePositionInBlocks;
+
+
+        //set this paddle ovject to saved position
+        this.gameObject.transform.position = paddlePosition;
+
+    }
+
+
+
+
+}
