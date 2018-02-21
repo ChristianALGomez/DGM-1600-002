92646ce89d26de642052318bb929b81583f71308
ChristianALGomez <10721401@my.uvu.edu>
2018-02-21 12:56:51 -0700

    added new lvls+win window LD array

--- BrickBreaker/Assets/Scripts/GameManger.cs
+++ BrickBreaker/Assets/Scripts/GameManger.cs
@@ -7,7 +7,9 @@ public class GameManger : MonoBehaviour
 {
 
     public static GameManger instance = null;
-
+    public static int BrickCount;
+    public static int BLC;
+    public GameObject[] LD;
 
     void Awake()
     {
@@ -22,16 +24,47 @@ public class GameManger : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
+        ChangeLD();
     }
+
+    public void ChangeLD()
+    {
+        //
+        //if current lvl is menu over/win turn it off
+        if (SceneManager.GetActiveScene().name == "Menu" || SceneManager.GetActiveScene().name == "GameOver" || SceneManager.GetActiveScene().name == "GameWin")
+        {
+            for (int i = 0; i < LD.Length; i++)
+            {
+                LD[i].SetActive(false);
+
+            }
+        }
+        //if current lvl is menu over/win turn it off
+        else
+        {
+            for (int i = 0; i < LD.Length; i++)
+            {
+                LD[i].SetActive(true);
+
+            }
+        }
+
+            //if display on lvl show it
+            //check life count
+        }
 
     public void LoadLevel(string level)
     {
+        BrickCount = 0;
         SceneManager.LoadScene(level);
+        //if scene is loaded
+        ChangeLD();
     }
 
-    public void Ending(string endscreen)
+    public void LoadNextLevel()
     {
-        SceneManager.LoadScene(endscreen);
+        BrickCount = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  //load next scene going up by 1
+        ChangeLD();
     }
-
 }
--- BrickBreaker/Assets/Scripts/HP.cs
+++ BrickBreaker/Assets/Scripts/HP.cs
@@ -6,16 +6,31 @@ public class HP : MonoBehaviour {
 
     public int health;
 
+
+    private void Awake()
+    {
+        GameManger.BrickCount++;
+        print(GameManger.BrickCount);
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         health--;
 
 
         //if our health gets to zero
-        if(health <= 0)
+        if (health <= 0)
         {
             //then destroy this object
             Destroy(gameObject);
+            GameManger.BrickCount--;
+            print(GameManger.BrickCount);
+
+            if(GameManger.BrickCount == 0)
+            {
+                FindObjectOfType<GameManger>().LoadNextLevel();
+            }
         }
     }
 
