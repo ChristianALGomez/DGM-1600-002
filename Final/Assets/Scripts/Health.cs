713b073575764c54ec5e1c04b20a7220989e2f85
ChristianALGomez <10721401@my.uvu.edu>
2018-03-12 10:15:27 -0600

    Particle system works with custom sprite, health script added for object

---
+++ Final/Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int health;
+    public ParticleSystem deathParticle;
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collided");
+        health--;
+        if(health <= 0)
+        {
+            Instantiate(deathParticle);
+
+            Destroy(gameObject);
+        }
+    }
+
+}
