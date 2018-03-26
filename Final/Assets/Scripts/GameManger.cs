8711afb76449348adbfd66948649dde6a3238669
ChristianALGomez <10721401@my.uvu.edu>
2018-03-26 10:10:47 -0600

    health needs work, creat level to play in

---
+++ Final/Assets/Scripts/GameManger.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour {
+
+    public static GameManger instance = null;
+    public GameObject[] LD;
+    public int HP;
+    private object currentScene;
+    public GameObject player;
+
+
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+            HP = 4;
+        }
+
+    }
+
+    void Awake()
+    {
+        //Singleton
+        if (instance == null)  //if instance is not assigned
+        {
+            instance = this;  //then assign instance to this object
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);  //then destroy this object
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void ChangeLD()
+    {
+        //
+        //if current lvl is menu over/win turn it off
+        if (SceneManager.GetActiveScene().name == "Menu" || SceneManager.GetActiveScene().name == "GameOver" || SceneManager.GetActiveScene().name == "GameWin")
+        {
+            for (int i = 0; i < LD.Length; i++)
+            {
+                LD[i].SetActive(false);
+
+            }
+        }
+        //if current lvl is menu over/win turn it off
+        else
+        {
+            for (int i = 0; i < LD.Length; i++)
+            {
+                LD[i].SetActive(false);
+                if (HP - 1 >= i)
+                {
+                    LD[i].SetActive(true);
+                }
+            }
+        }
+    }
+    public void LoadNextLevel()
+    {
+       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  //load next scene going up by 1
+    }
+}
--- Final/Assets/Scripts/PlayerMovement.cs
+++ Final/Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour {
     public bool Grounded = true;
     private Animator anim;
     private SpriteRenderer rend;
+    public LayerMask groundLayer;
 
 
 	// Use this for initialization
@@ -22,13 +23,13 @@ public class PlayerMovement : MonoBehaviour {
         anim = GetComponent<Animator>();
         rend = GetComponent<SpriteRenderer>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    // Update is called once per frame
+    void Update () {
+
         //check for button pushes
-
-        
-
+        //player movment
         rigid.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0), ForceMode2D.Force);
         anim.SetFloat("HorizontalGo", Input.GetAxisRaw("Horizontal"));
         if(Input.GetAxisRaw("Horizontal") < -0.1f)
@@ -40,14 +41,53 @@ public class PlayerMovement : MonoBehaviour {
             //unflip
             rend.flipX = false;
         }
+        //attack animation by clicking right button or ctrl
         if (Input.GetButton("Fire1"))
         {
             anim.SetTrigger("FireGo");
         }
-        if (Grounded && Input.GetButtonDown("Jump"))
+        //Jump
+        if (Input.GetButtonDown("Jump"))
         {
-            rigid.AddForce(new Vector2(0, jumpPower), ForceMode2D.Force);
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 10.0f, 9);
+            if (hit.collider != null) {
+                
+            if (hit.collider.tag == "Ground")
+                {
+                    Grounded = true;
+                    Debug.Log("found ground");
+                }
+                else
+                {
+                    Grounded = false;
+                    Debug.Log("no ground");
+                }
+            }
+
+            if (Grounded)
+            {
+                rigid.AddForce(new Vector2(0, jumpPower), ForceMode2D.Force);
+            }
         }
+    }
+
+
+    bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 1.0f;
+
+        Debug.DrawRay(position, direction, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
