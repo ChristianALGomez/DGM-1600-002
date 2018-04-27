f964e068d695668c4fcf6702b220a06080685a2a
ChristianALGomez <10721401@my.uvu.edu>
2018-04-27 17:17:05 -0600

    created gate lvl 1 complete lvl 2 needs work, no lvl 3

--- Final/Assets/Scripts/Win.cs
+++ Final/Assets/Scripts/Win.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Win : MonoBehaviour
-{
+public class Win : MonoBehaviour {
 
     public Manager theGameManger;
     public Holder EndTime;
@@ -17,7 +16,9 @@ public class Win : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         theGameManger.LoadLevel("GameWin");
         EndTime.Finished();
+
     }
 }
